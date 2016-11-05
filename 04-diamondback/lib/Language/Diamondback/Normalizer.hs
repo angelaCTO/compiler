e5c174b112e4aba9c7b542d42dc92b898174ab98
cshmerli@ucsd.edu <cshmerli@ucsd.edu>
2016-11-04 18:53:20 -0700

    more

--- 04-diamondback/lib/Language/Diamondback/Normalizer.hs
+++ 04-diamondback/lib/Language/Diamondback/Normalizer.hs
@@ -58,7 +58,9 @@ anf i (If c e1 e2 l)    = (i''', stitch bs  (If c' e1' e2' l))
     (i'' ,     e1')     = anf i'  e1
     (i''',     e2')     = anf i'' e2
 
-anf i (App f es l)      = imm i (App f es l)
+anf i (App f es l)      = (i', stitch bs (App f es l)) 
+  where
+  (i', bs, es') = imm i (App f es l)
 
 --------------------------------------------------------------------------------
 -- | `stitch bs e` takes a "context" `bs` which is a list of temp-vars and their
@@ -111,8 +113,11 @@ imm i (Prim2 o e1 e2 l) = (i'', bs', mkId x l)
     (i'', x)            = fresh l i'
     bs'                 = (x, (Prim2 o v1 v2 l, l)) : bs
 
-imm i (App f es l)      =  
-
+imm i (App f es l)      =  (i'', bs', mkId x l)
+  where
+   (i', bs, es') = imms i es
+   (i'', x)      = fresh l i'
+   bs'           = (x, (App f es' l, l)) : bs
 imm i e@(If _ _ _  l)   = immExp i e l
 
 imm i e@(Let _ _ _ l)   = immExp i e l
